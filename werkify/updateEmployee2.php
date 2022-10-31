412c0a55a375a25beee1670bf2b5e396827948ae
Kae Flizzy <kaeflizzy@gmal.com>
2022-10-31 16:21:07 +0300

    Werkify

--- werkify/updateEmployee2.php
+++ werkify/updateEmployee2.php
@@ -1,10 +1,22 @@
 <?php
 include_once 'db_conn.php';
 if(count($_POST)>0) {
-mysqli_query($conn,"UPDATE employee set empID='" . $_POST['empID'] ."', name='" . $_POST['name'] ."', username='" . $_POST['username'] . "', job='" . $_POST['job'] . "',department='" . $_POST['department'] . "' WHERE empID='" . $_POST['empID'] . "'");
+mysqli_query($conn,"UPDATE employee set 
+empID= '" . $_POST['empID'] ."',
+name='" . $_POST['name'] ."',
+ username='" . $_POST['username'] . "',
+  gender='" . $_POST['gender'] ."', 
+  dob='" . $_POST['dob'] ."', 
+  nin='" . $_POST['nin'] ."', 
+  Address='" . $_POST['Address'] ."', 
+  Job='" . $_POST['Job'] . "',
+  Department='" . $_POST['Department'] . "',
+  education='" . $_POST['education'] . "',
+  account='" . $_POST['account'] . "',
+  WHERE empID='" . $_POST['empID'] . "'");
 $message = "Record Modified Successfully";
 }
-$sql="SELECT empID, name, username,job,department FROM employee WHERE empID = '" . $_GET['id'] . "'";
+$sql="SELECT * FROM employee WHERE empID = '" . $_GET['id'] . "'";
 $result = mysqli_query($conn,$sql);
 $row= mysqli_fetch_array($result);
 ?>
@@ -24,16 +36,34 @@ empID: <br>
 <input type="text" name="empID"  value="<?php echo $row['empID']; ?>">
 <br>
 Full Name : <br>
-<input type="text" name="name" class="txtField" value="<?php echo $row['name']; ?>">
+<input type="text" name="name" class="txtField" value="<?php echo $row['Name']; ?>">
 <br>
 Username : <br>
 <input type="text" name="username" class="txtField" value="<?php echo $row['username']; ?>">
 <br>
+Gender: <br>
+<input type="text" name="gender" class="txtField" value="<?php echo $row['Gender']; ?>">
+<br>
+Date of Birth: <br>
+<input type="date" name="dob" class="txtField" value="<?php echo $row['DOB']; ?>">
+<br>
+Address: <br>
+<input type="text" name="Address" class="txtField" value="<?php echo $row['Address']; ?>">
+<br>
+NIN: <br>
+<input type="text" name="nin" class="txtField" value="<?php echo $row['NIN']; ?>">
+<br>
 Job Title :<br>
-<input type="text" name="job" class="txtField" value="<?php echo $row['job']; ?>">
+<input type="text" name="Job" class="txtField" value="<?php echo $row['Job']; ?>">
 <br>
 Department :<br>
-<input type="text" name="department" class="txtField" value="<?php echo $row['department']; ?>">
+<input type="text" name="Department" class="txtField" value="<?php echo $row['Department']; ?>">
+<br>
+Education Level :<br>
+<input type="text" name="education" class="txtField" value="<?php echo $row['Education']; ?>">
+<br>
+Account Number :<br>
+<input type="text" name="account" class="txtField" value="<?php echo $row['Account']; ?>">
 <br>
 
 <input type="submit" name="submit" value="Submit" class="button">
